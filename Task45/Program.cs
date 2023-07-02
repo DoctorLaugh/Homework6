23ce1ebba309c67e875cef459d5342e2b2dd354b
Dmitriy <trayan@yandex.ru>
2023-07-02 19:24:54 +1100

    Task45 added

---
+++ Task45/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] GetArray(string[] input)
+{
+    int[] array = new int[input.Length];
+    for(int i = 0; i < input.Length; i++)
+    {
+        array[i] = int.Parse(input[i]);
+    }
+    return array;
+}
+int[] CopyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите числа массива через пробел: ");
+string[] InputArray = Console.ReadLine().Split(' ');
+int[] Array = GetArray(InputArray);
+Console.Write($"Исходный массив: {String.Join(" ", Array)}");
+Console.WriteLine();
+int[] NewArray = CopyArray(Array);
+Console.Write($"Копия массива: {String.Join(" ",NewArray)}");
